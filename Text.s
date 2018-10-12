268e1f69784e0e27d9862207786d4694e65a03e8
jamalx61 <44097824+jamalx61@users.noreply.github.com>
2018-10-12 13:39:06 -0400

    Update Text.s
    Hello

---
+++ Text.s
@@ -0,0 +1,18 @@
+#Jamal's MIP!!!!!!!!!!!!
+# Demo
+#FileName: LoadImmediate
+
+
+
+	.data	
+
+	.text
+	# Load some registers
+	li	$t0, 0x2      #Load Immediate
+	li	$t1, 0x3
+	li	$t2, 0x4
+	li	$t3, 0x56
+	li	$t4, 0x90
+
+	# Exit the Program
+	
