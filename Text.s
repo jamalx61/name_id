f2e32df5aa7847fba28aef182fb4358e5429e25b
jamalx61 <44097824+jamalx61@users.noreply.github.com>
2018-10-15 17:27:17 -0400

    Update Text.s
    Line 20  calls the code to print an interger
    line 21 loads up the value tht will be printed
    syscall excecutes
    
    line 25 load a empty address
    27- calls the printing for a string function
    28 excecutes

--- Text.s
+++ Text.s
@@ -6,16 +6,27 @@
 
 	.data	
 	# Load some registers
-	symbol:		.asciiz	"@"
+	symbol:		.asciiz	"@"       #Strings are defined with .ascii or .asciiz directives.
 	id:    		.word	02829293
 	name:  		.asciiz	"Kamara, Abdul"
 	.text
 	
 	.globl		main
 	main:
+
+
 		
-		la	$
+		
+		li 	   $v0, 1			 # call code, print int
+		lw         $a0, id 			 # value for int to print
+		syscall 			# system call
+
+
+		la        $a0, name         # addr of NULL
+		
+		li         $v0, 4                  # call code, print string
+		syscall 			# system call
 	
 
-	# Exit the Program
+	
 	
