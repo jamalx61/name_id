6da2f074132628ac8e4d7070ae5d0ee793c1f079
jamalx61 <44097824+jamalx61@users.noreply.github.com>
2018-10-18 15:17:43 -0400

    Update Text.s
    30 ) Gets the decimal for the leter A
    31) calls the system to excute the prvious line
    32) Gets the decimal for a comma
    33)  calls the system to excute the prvious line
    34) Creates a space in between the last and first name
    35) calls the system to excute the prvious line

--- Text.s
+++ Text.s
@@ -17,9 +17,9 @@
 
 		
                 li        $v0, 11	        # print character call
-		la        $a0, 75 		# K
+		la        $a0, 75 		# K 
 		syscall     
-		addi       $a0, $a0, -10	# A
+		addi       $a0, $a0, -10	# A 
 		syscall   
 		addi	  $a0, $a0,  12		# M                 
 		syscall     
@@ -27,3 +27,12 @@
 		syscall     
 		addi 	  $a0, $a0, 17		# R
 		syscall
+		addi	  $a0, $a0, -17		#A
+		syscall     
+		addi	  $a0, $a0, -21		#,// 44
+		syscall     
+		addi	  $a0, $a0, -12 	#space //42
+		syscall 
+		
+
+    
