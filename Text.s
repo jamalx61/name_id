1352e9318327abd3e5479968ceae605adab16220
jamalx61 <44097824+jamalx61@users.noreply.github.com>
2018-10-19 20:44:35 -0400

    Update Text.s
    56) this line will make the decimal form call the letter A
    57) Will excecute the previous  line to print A
    58)this line will make the decimal form call the letter
    59)Will excecute the previous  line to print B
    60)this line will make the decimal form call the letter
    61)Will excecute the previous  line to print D

--- Text.s
+++ Text.s
@@ -17,9 +17,29 @@
 
 		
                 li        $v0, 11	        # print character call
-		la        $a0, 75 		# K 
+		la        $a0, 64                #@
+		syscall
+ 		addi       $a0, $a0, -16	# 0 
+		syscall  
+		addi       $a0, $a0, 2 		# 2 
+		syscall 
+		 addi       $a0, $a0, 6		# 8 
+		syscall  
+		addi	   $a0, $a0, -6		# 2
+		syscall  
+		addi       $a0, $a0, 7		# 9
+		syscall
+		addi       $a0, $a0, -7		# 2
+		syscall  		
+		addi        $a0, $a0, 7		# 9 
 		syscall     
-		addi       $a0, $a0, -10	# A 
+		addi       $a0, $a0, -6		# 3
+		syscall   
+		addi       $a0, $a0, -41	# nextline
+		syscall  
+		addi	   $a0, $a0, 65		# K //Update
+		syscall   
+		addi       $a0, $a0, -10	# A
 		syscall   
 		addi	  $a0, $a0,  12		# M                 
 		syscall     
@@ -33,6 +53,14 @@
 		syscall     
 		addi	  $a0, $a0, -12 	#space //42
 		syscall 
+		addi	  $a0, $a0, 33	#space //A
+		syscall 
+		addi	  $a0, $a0, 1 	#space //B
+		syscall 
+		addi	  $a0, $a0, 2	#space //D
+		syscall 
+		
 		
 
+		
     
